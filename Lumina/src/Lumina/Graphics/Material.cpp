5aa3d4399a880b74550b816809e30e3d5735b3ef
stephen-os <ImStephen.1sw@gmail.com>
2025-07-06 17:51:47 -0400

    Added more attributes like emission, height scale and alpha.

--- Lumina/src/Lumina/Graphics/Material.cpp
+++ Lumina/src/Lumina/Graphics/Material.cpp
@@ -6,6 +6,7 @@
 
 namespace Lumina
 {
+
     void Material::BindMaterial(Ref<ShaderProgram> shader, Ref<Material> material, Ref<Texture> whiteTexture, Ref<Texture> normalMap)
     {
         LUMINA_ASSERT(shader, "Material: Shader cannot be null");
@@ -13,7 +14,7 @@ namespace Lumina
         LUMINA_ASSERT(normalMap, "Material: Normal map cannot be null");
 
         if (!material)
-            return; 
+            return;
 
         if (material->GetAlbedoTexture())
         {
@@ -41,6 +42,7 @@ namespace Lumina
             shader->SetUniformInt("u_HasNormalTexture", false);
         }
 
+        // Bind Metallic Texture
         if (material->GetMetallicTexture())
         {
             material->GetMetallicTexture()->Bind(TextureSlots::MATERIAL_METALLIC);
@@ -54,6 +56,7 @@ namespace Lumina
             shader->SetUniformInt("u_HasMetallicTexture", false);
         }
 
+        // Bind Roughness Texture
         if (material->GetRoughnessTexture())
         {
             material->GetRoughnessTexture()->Bind(TextureSlots::MATERIAL_ROUGHNESS);
@@ -67,6 +70,7 @@ namespace Lumina
             shader->SetUniformInt("u_HasRoughnessTexture", false);
         }
 
+        // Bind AO Texture
         if (material->GetAOTexture())
         {
             material->GetAOTexture()->Bind(TextureSlots::MATERIAL_AO);
@@ -80,19 +84,78 @@ namespace Lumina
             shader->SetUniformInt("u_HasAOTexture", false);
         }
 
+        // Bind Emission Texture
+        if (material->GetEmissionTexture())
+        {
+            material->GetEmissionTexture()->Bind(TextureSlots::MATERIAL_EMISSION);
+            shader->SetUniformInt("u_EmissionTexture", TextureSlots::MATERIAL_EMISSION);
+            shader->SetUniformInt("u_HasEmissionTexture", true);
+        }
+        else
+        {
+            whiteTexture->Bind(TextureSlots::MATERIAL_EMISSION);
+            shader->SetUniformInt("u_EmissionTexture", TextureSlots::MATERIAL_EMISSION);
+            shader->SetUniformInt("u_HasEmissionTexture", false);
+        }
+
+        // Bind Height Texture
+        if (material->GetHeightTexture())
+        {
+            material->GetHeightTexture()->Bind(TextureSlots::MATERIAL_HEIGHT);
+            shader->SetUniformInt("u_HeightTexture", TextureSlots::MATERIAL_HEIGHT);
+            shader->SetUniformInt("u_HasHeightTexture", true);
+        }
+        else
+        {
+            whiteTexture->Bind(TextureSlots::MATERIAL_HEIGHT);
+            shader->SetUniformInt("u_HeightTexture", TextureSlots::MATERIAL_HEIGHT);
+            shader->SetUniformInt("u_HasHeightTexture", false);
+        }
+
+        // Bind Alpha Texture
+        if (material->GetAlphaTexture())
+        {
+            material->GetAlphaTexture()->Bind(TextureSlots::MATERIAL_ALPHA);
+            shader->SetUniformInt("u_AlphaTexture", TextureSlots::MATERIAL_ALPHA);
+            shader->SetUniformInt("u_HasAlphaTexture", true);
+        }
+        else
+        {
+            whiteTexture->Bind(TextureSlots::MATERIAL_ALPHA);
+            shader->SetUniformInt("u_AlphaTexture", TextureSlots::MATERIAL_ALPHA);
+            shader->SetUniformInt("u_HasAlphaTexture", false);
+        }
+
+        // Set Material Properties
         if (material)
         {
+            // Base PBR Properties
             shader->SetUniformVec3("u_Material.Albedo", material->GetAlbedo());
             shader->SetUniformFloat("u_Material.Metallic", material->GetMetallic());
             shader->SetUniformFloat("u_Material.Roughness", material->GetRoughness());
             shader->SetUniformFloat("u_Material.AO", material->GetAO());
+
+            // New Properties
+            shader->SetUniformVec3("u_Material.Emission", material->GetEmission());
+            shader->SetUniformFloat("u_Material.EmissionIntensity", material->GetEmissionIntensity());
+            shader->SetUniformFloat("u_Material.HeightScale", material->GetHeightScale());
+            shader->SetUniformFloat("u_Material.Alpha", material->GetAlpha());
+            shader->SetUniformFloat("u_Material.NormalIntensity", material->GetNormalIntensity());
         }
         else
         {
+            // Default Material Properties
             shader->SetUniformVec3("u_Material.Albedo", glm::vec3(1.0f));
             shader->SetUniformFloat("u_Material.Metallic", 0.0f);
             shader->SetUniformFloat("u_Material.Roughness", 0.5f);
             shader->SetUniformFloat("u_Material.AO", 1.0f);
+
+            // Default New Properties
+            shader->SetUniformVec3("u_Material.Emission", glm::vec3(0.0f));
+            shader->SetUniformFloat("u_Material.EmissionIntensity", 1.0f);
+            shader->SetUniformFloat("u_Material.HeightScale", 0.05f);
+            shader->SetUniformFloat("u_Material.Alpha", 1.0f);
+            shader->SetUniformFloat("u_Material.NormalIntensity", 1.0f);
         }
     }
 
--- Lumina/src/Lumina/Graphics/Material.h
+++ Lumina/src/Lumina/Graphics/Material.h
@@ -35,6 +35,21 @@ namespace Lumina
         void SetAO(float ao) { m_AO = ao; }
         float GetAO() const { return m_AO; }
 
+        void SetEmission(const glm::vec3& emission) { m_Emission = emission; }
+        const glm::vec3& GetEmission() const { return m_Emission; }
+
+        void SetEmissionIntensity(float intensity) { m_EmissionIntensity = intensity; }
+        float GetEmissionIntensity() const { return m_EmissionIntensity; }
+
+        void SetHeightScale(float scale) { m_HeightScale = scale; }
+        float GetHeightScale() const { return m_HeightScale; }
+
+        void SetAlpha(float alpha) { m_Alpha = alpha; }
+        float GetAlpha() const { return m_Alpha; }
+
+        void SetNormalIntensity(float intensity) { m_NormalIntensity = intensity; }
+        float GetNormalIntensity() const { return m_NormalIntensity; }\
+
         void SetAlbedoTexture(const Ref<Texture>& texture) { m_AlbedoTexture = texture; }
         Ref<Texture> GetAlbedoTexture() const { return m_AlbedoTexture; }
 
@@ -50,6 +65,16 @@ namespace Lumina
         void SetAOTexture(const Ref<Texture>& texture) { m_AOTexture = texture; }
         Ref<Texture> GetAOTexture() const { return m_AOTexture; }
 
+        void SetEmissionTexture(const Ref<Texture>& texture) { m_EmissionTexture = texture; }
+        Ref<Texture> GetEmissionTexture() const { return m_EmissionTexture; }
+
+        void SetHeightTexture(const Ref<Texture>& texture) { m_HeightTexture = texture; }
+        Ref<Texture> GetHeightTexture() const { return m_HeightTexture; }
+
+        void SetAlphaTexture(const Ref<Texture>& texture) { m_AlphaTexture = texture; }
+        Ref<Texture> GetAlphaTexture() const { return m_AlphaTexture; }
+
+
         void SetName(const std::string& name) { m_Name = name; }
         const std::string& GetName() const { return m_Name; }
 
@@ -62,11 +87,19 @@ namespace Lumina
         float m_Metallic = 0.0f;
         float m_Roughness = 0.5f;
         float m_AO = 1.0f;
+        glm::vec3 m_Emission = glm::vec3(0.0f, 0.0f, 0.0f);
+        float m_EmissionIntensity = 1.0f;
+        float m_HeightScale = 0.05f;
+        float m_Alpha = 1.0f;
+        float m_NormalIntensity = 1.0f;
 
         Ref<Texture> m_AlbedoTexture = nullptr;
         Ref<Texture> m_NormalTexture = nullptr;
         Ref<Texture> m_MetallicTexture = nullptr;
         Ref<Texture> m_RoughnessTexture = nullptr;
         Ref<Texture> m_AOTexture = nullptr;
+        Ref<Texture> m_EmissionTexture = nullptr;
+        Ref<Texture> m_HeightTexture = nullptr;
+        Ref<Texture> m_AlphaTexture = nullptr;
     };
 }
