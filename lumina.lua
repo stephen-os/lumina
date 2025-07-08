a09fd1fe01bfaaaee02606c5785a10d70be8ed22
resetss <imstephen.1sw@gmail.com>
2025-07-08 11:49:00 -0400

    Project clean up. Renamed files and folders for consistancy. Removed orbital-engine project.

---
+++ lumina.lua
@@ -0,0 +1,18 @@
+-- lumina.lua
+workspace "Lumina"
+   architecture "x64"
+   configurations { "Debug", "Release", "Dist" }
+   startproject "Example"
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+-- Lumina External
+include "lumina-external.lua"
+
+-- Projects
+group "Projects"
+   include "projects/example/example.lua"
+   include "projects/basic-rendering/basic-rendering.lua"
+   include "projects/renderer-2d/renderer-2d.lua"
+   include "projects/renderer-3d/renderer-3d.lua"
+group ""
--- scripts/Setup.bat
+++ scripts/Setup.bat
@@ -1,6 +1,6 @@
 @echo off
 
 pushd ..
-Dependencies\premake\premake5.exe --file=premake5.lua vs2022
+Dependencies\premake\premake5.exe --file=lumina.lua vs2022
 popd
 pause
