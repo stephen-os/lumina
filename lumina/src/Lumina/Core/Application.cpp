0a1ccd73d8ebe5067dbfe4335c17cd4ed911bce9
resetss <imstephen.1sw@gmail.com>
2025-09-23 09:22:19 -0400

    Fixed bug with imgui not creating a new window when dockspace was dissabled.

--- lumina/src/Lumina/Core/Application.cpp
+++ lumina/src/Lumina/Core/Application.cpp
@@ -128,37 +128,38 @@ namespace Lumina
             ImGui_ImplGlfw_NewFrame();
             ImGui::NewFrame();
 
+            ImGuiViewport* viewport = ImGui::GetMainViewport();
+            ImGui::SetNextWindowPos(viewport->WorkPos);
+            ImGui::SetNextWindowSize(viewport->WorkSize);
+            ImGui::SetNextWindowViewport(viewport->ID);
+
+            ImGuiWindowFlags window_flags =
+                ImGuiWindowFlags_NoTitleBar |
+                ImGuiWindowFlags_NoCollapse |
+                ImGuiWindowFlags_NoResize |
+                ImGuiWindowFlags_NoMove |
+                ImGuiWindowFlags_NoBringToFrontOnFocus |
+                ImGuiWindowFlags_NoNavFocus |
+                ImGuiWindowFlags_NoBackground;
+
+            ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+            ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+            ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+
+            ImGui::Begin("LuminaWindow", nullptr, window_flags);
+            
+            ImGui::PopStyleVar(3);
+
             if (m_Specifications.EnableDocking)
             {
-                ImGuiViewport* viewport = ImGui::GetMainViewport();
-                ImGui::SetNextWindowPos(viewport->WorkPos);
-                ImGui::SetNextWindowSize(viewport->WorkSize);
-                ImGui::SetNextWindowViewport(viewport->ID);
-
-                ImGuiWindowFlags dockspace_flags =
-                    ImGuiWindowFlags_NoTitleBar |
-                    ImGuiWindowFlags_NoCollapse |
-                    ImGuiWindowFlags_NoResize |
-                    ImGuiWindowFlags_NoMove |
-                    ImGuiWindowFlags_NoBringToFrontOnFocus |
-                    ImGuiWindowFlags_NoNavFocus |
-                    ImGuiWindowFlags_NoBackground;
-
-                ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
-                ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
-                ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
-
-                ImGui::Begin("DockSpace", nullptr, dockspace_flags);
-
-                m_DockspaceID = ImGui::GetID("MainDockSpace");
+                m_DockspaceID = ImGui::GetID("LuminaDockspace");
                 ImGui::DockSpace(m_DockspaceID, ImVec2(0.0f, 0.0f), ImGuiDockNodeFlags_None);
-
-                ImGui::End();
-                ImGui::PopStyleVar(3);
             }
 
             for (auto& layer : m_LayerStack)
                 layer->OnUIRender();
+
+            ImGui::End();
 
             ImGui::Render();
 
