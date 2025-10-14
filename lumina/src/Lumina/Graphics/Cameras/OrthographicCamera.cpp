eb7e266cca0ea9dbdf0612d881c9cc1cd238f72c
resetss <imstephen.1sw@gmail.com>
2025-10-13 22:32:38 -0400

    Attempting to fix this.

--- lumina/src/Lumina/Graphics/Cameras/OrthographicCamera.cpp
+++ lumina/src/Lumina/Graphics/Cameras/OrthographicCamera.cpp
@@ -14,12 +14,6 @@ namespace Lumina
 		m_Right = right;
 		m_Bottom = bottom;
 		m_Top = top;
-		
-        m_BaseLeft = left;
-		m_BaseRight = right;
-		m_BaseBottom = bottom;
-		m_BaseTop = top;
-
 		m_NearPlane = nearPlane;
         m_FarPlane = farPlane; 
 
@@ -39,10 +33,7 @@ namespace Lumina
         m_Right = right;
         m_Bottom = bottom;
         m_Top = top;
-        m_BaseLeft = left;
-        m_BaseRight = right;
-        m_BaseBottom = bottom;
-        m_BaseTop = top;
+
         UpdateProjectionMatrix();
     }
 
@@ -50,45 +41,6 @@ namespace Lumina
     {
         m_NearPlane = nearPlane;
         m_FarPlane = farPlane;
-        UpdateProjectionMatrix();
-    }
-
-    void OrthographicCamera::SetViewportSize(float width, float height)
-    {
-        float aspectRatio = width / height;
-
-        float baseHeight = m_Top - m_Bottom;
-        float viewHeight = baseHeight / m_Zoom;
-        float viewWidth = viewHeight * aspectRatio;
-
-        m_Left = -viewWidth * 0.5f;
-        m_Right = viewWidth * 0.5f;
-        m_Bottom = -viewHeight * 0.5f;
-        m_Top = viewHeight * 0.5f;
-
-        m_BaseLeft = m_Left * m_Zoom;
-        m_BaseRight = m_Right * m_Zoom;
-        m_BaseBottom = m_Bottom * m_Zoom;
-        m_BaseTop = m_Top * m_Zoom;
-
-        UpdateProjectionMatrix();
-    }
-
-    void OrthographicCamera::SetZoom(float zoom)
-    {
-        m_Zoom = std::max(zoom, 0.001f);
-
-        float width = (m_BaseRight - m_BaseLeft) / m_Zoom;
-        float height = (m_BaseTop - m_BaseBottom) / m_Zoom;
-
-        float centerX = (m_BaseLeft + m_BaseRight) * 0.5f;
-        float centerY = (m_BaseBottom + m_BaseTop) * 0.5f;
-
-        m_Left = centerX - width * 0.5f;
-        m_Right = centerX + width * 0.5f;
-        m_Bottom = centerY - height * 0.5f;
-        m_Top = centerY + height * 0.5f;
-
         UpdateProjectionMatrix();
     }
 
--- lumina/src/Lumina/Graphics/Cameras/OrthographicCamera.h
+++ lumina/src/Lumina/Graphics/Cameras/OrthographicCamera.h
@@ -15,8 +15,6 @@ namespace Lumina
         void SetPosition(const glm::vec3& position) override;
         void SetBounds(float left, float right, float bottom, float top);
         void SetClippingPlanes(float nearPlane, float farPlane);
-        void SetViewportSize(float width, float height);
-        void SetZoom(float zoom);
 
         float GetLeft() const { return m_Left; }
         float GetRight() const { return m_Right; }
@@ -24,10 +22,9 @@ namespace Lumina
         float GetTop() const { return m_Top; }
         float GetNearPlane() const { return m_NearPlane; }
         float GetFarPlane() const { return m_FarPlane; }
-        float GetZoom() const { return m_Zoom; }
         float GetWidth() const { return m_Right - m_Left; }
-        float GetHeight() const { return m_Top - m_Bottom; }
-
+		float GetHeight() const { return m_Top - m_Bottom; }
+        
     protected:
         void UpdateViewMatrix() override;
         void UpdateProjectionMatrix() override;
@@ -39,11 +36,5 @@ namespace Lumina
         float m_Top;
         float m_NearPlane;
         float m_FarPlane;
-
-        float m_Zoom = 1.0f;
-        float m_BaseLeft;
-        float m_BaseRight;
-        float m_BaseBottom;
-        float m_BaseTop;
     };
 }
