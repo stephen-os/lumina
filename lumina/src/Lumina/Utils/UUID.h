60db2fdbb3511a32a9c6730b23af5fd3e3b28d1a
stephen-os <ImStephen.1sw@gmail.com>
2025-11-12 20:32:26 -0500

    Moved implementation of generate to implementation.

--- lumina/src/Lumina/Utils/UUID.h
+++ lumina/src/Lumina/Utils/UUID.h
@@ -2,27 +2,15 @@
 
 #include <chrono>
 #include <random>
-
 #include <mutex>
 
 namespace Lumina
 {
-	class UUID
-	{
-	public:
-		static uint64_t Generate()
-		{
-			std::lock_guard<std::mutex> lock(m_Mutex);
-
-			auto now = std::chrono::high_resolution_clock::now().time_since_epoch().count();
-			std::random_device rd;
-			std::mt19937_64 gen(rd());
-			uint64_t randomValue = gen();
-			return now ^ randomValue;
-		}
-	private:
-		static std::mutex m_Mutex; 
-	};
-
-	std::mutex UUID::m_Mutex; 
+    class UUID
+    {
+    public:
+        static uint64_t Generate();
+    private:
+        static std::mutex m_Mutex;
+    };
 }
