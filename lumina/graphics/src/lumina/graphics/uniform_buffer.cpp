62508c4dcc5dacd7658b5acd4022729c3e2c2d57
Stephen Watson <imstephen.1sw@gmail.com>
2026-02-02 09:14:35 -0500

    feat: added uniform buffer

---
+++ lumina/graphics/src/lumina/graphics/uniform_buffer.cpp
@@ -0,0 +1,83 @@
+#include "uniform_buffer.h"
+#include "device.h"
+
+#include <lumina/core/assert.h>
+#include <lumina/core/log.h>
+
+#include <nvrhi/nvrhi.h>
+
+namespace lumina::graphics
+{
+    uniform_buffer::~uniform_buffer()
+    {
+        if (m_handle)
+        {
+            m_handle->Release();
+            m_handle = nullptr;
+        }
+    }
+
+    uniform_buffer::uniform_buffer(uniform_buffer&& other) noexcept
+        : m_device(other.m_device)
+        , m_handle(other.m_handle)
+        , m_size(other.m_size)
+        , m_aligned_size(other.m_aligned_size)
+    {
+        other.m_handle = nullptr;
+        other.m_size = 0;
+        other.m_aligned_size = 0;
+    }
+
+    ref<uniform_buffer> uniform_buffer::create(device& dev, size_t size)
+    {
+        auto* nvrhi_device = dev.get_nvrhi_device();
+        if (!nvrhi_device)
+        {
+            LUMINA_LOG_ERROR("Failed to create uniform buffer: no device");
+            return nullptr;
+        }
+
+        size_t aligned_size = (size + 255) & ~255;
+
+        nvrhi::BufferDesc desc;
+        desc.byteSize = aligned_size;
+        desc.isConstantBuffer = true;
+        desc.debugName = "Lumina Uniform Buffer";
+        desc.initialState = nvrhi::ResourceStates::CopyDest;
+        desc.keepInitialState = false;
+
+        nvrhi::BufferHandle buffer = nvrhi_device->createBuffer(desc);
+        if (!buffer)
+        {
+            LUMINA_LOG_ERROR("Failed to create NVRHI uniform buffer");
+            return nullptr;
+        }
+
+        buffer->AddRef();
+
+        return ref<uniform_buffer>(new uniform_buffer(dev, buffer.Get(), size, aligned_size));
+    }
+
+    void uniform_buffer::update(const void* data, size_t size, nvrhi::ICommandList* cmd_list)
+    {
+        LUMINA_ASSERT(data != nullptr, "Cannot update uniform buffer with null data");
+        LUMINA_ASSERT(cmd_list != nullptr, "Command list is required for uniform buffer update");
+        LUMINA_ASSERT(m_handle != nullptr, "Uniform buffer handle is null");
+
+        if (!data || !cmd_list || !m_handle)
+        {
+            LUMINA_LOG_ERROR("uniform_buffer::update called with invalid state");
+            return;
+        }
+
+        if (size > m_size)
+        {
+            LUMINA_LOG_WARN("Update size {} exceeds buffer size {}", size, m_size);
+            size = m_size;
+        }
+
+        cmd_list->writeBuffer(m_handle, data, size);
+
+        cmd_list->setBufferState(m_handle, nvrhi::ResourceStates::ConstantBuffer);
+    }
+}
---
+++ lumina/graphics/src/lumina/graphics/uniform_buffer.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include "types.h"
+
+#include <lumina/core/base.h>
+
+#include <cstddef>
+
+namespace nvrhi
+{
+    class IBuffer;
+    class ICommandList;
+}
+
+namespace lumina::graphics
+{
+    class device;
+
+    class uniform_buffer
+    {
+    public:
+        ~uniform_buffer();
+
+        uniform_buffer(const uniform_buffer&) = delete;
+        uniform_buffer& operator=(const uniform_buffer&) = delete;
+        uniform_buffer(uniform_buffer&& other) noexcept;
+        uniform_buffer& operator=(uniform_buffer&&) = delete;
+
+        static ref<uniform_buffer> create(device& dev, size_t size);
+
+        template<typename T>
+        static ref<uniform_buffer> create(device& dev)
+        {
+            return create(dev, sizeof(T));
+        }
+
+        void update(const void* data, size_t size, nvrhi::ICommandList* cmd_list);
+
+        template<typename T>
+        void update(const T& data, nvrhi::ICommandList* cmd_list)
+        {
+            update(&data, sizeof(T), cmd_list);
+        }
+
+        size_t get_size() const { return m_size; }
+        size_t get_aligned_size() const { return m_aligned_size; }
+
+        nvrhi::IBuffer* get_buffer() const { return m_handle; }
+
+    private:
+        uniform_buffer(device& dev, nvrhi::IBuffer* handle, size_t size, size_t aligned_size)
+            : m_device(dev)
+            , m_handle(handle)
+            , m_size(size)
+            , m_aligned_size(aligned_size)
+        {}
+
+        device& m_device;
+        nvrhi::IBuffer* m_handle;
+        size_t m_size;
+        size_t m_aligned_size;
+    };
+}
