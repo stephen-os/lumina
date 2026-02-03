4229c1102b07264bdc374429c21658cfdccf2279
Stephen Watson <imstephen.1sw@gmail.com>
2026-02-03 11:42:56 -0500

    feat: added timer utility

---
+++ lumina/core/src/lumina/core/timer.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "log.h"
+
+#include <chrono>
+#include <string>
+
+namespace lumina::core
+{
+    class timer
+    {
+    public:
+        timer() { reset(); }
+
+        void reset()
+        {
+            m_start = std::chrono::high_resolution_clock::now();
+        }
+
+        float elapsed() const
+        {
+            auto now = std::chrono::high_resolution_clock::now();
+            return std::chrono::duration<float>(now - m_start).count();
+        }
+
+        float elapsed_millis() const
+        {
+            return elapsed() * 1000.0f;
+        }
+
+    private:
+        std::chrono::time_point<std::chrono::high_resolution_clock> m_start;
+    };
+
+    class scoped_timer
+    {
+    public:
+        explicit scoped_timer(const std::string& name)
+            : m_name(name) {}
+
+        ~scoped_timer()
+        {
+            float ms = m_timer.elapsed_millis();
+            LUMINA_LOG_TRACE("[TIMER] {} - {:.3f}ms", m_name, ms);
+        }
+
+        scoped_timer(const scoped_timer&) = delete;
+        scoped_timer& operator=(const scoped_timer&) = delete;
+
+    private:
+        std::string m_name;
+        timer m_timer;
+    };
+}
