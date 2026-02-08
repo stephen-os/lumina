d337b204e0c7c3b68bc819f37b290c8e5f30e16d
Stephen Watson <imstephen.1sw@gmail.com>
2026-02-07 21:10:43 -0500

    feat: added graphics header.

---
+++ lumina/graphics/src/lumina/graphics/graphics.h
@@ -0,0 +1,18 @@
+#pragma once
+
+// Lumina Graphics Module
+// Low-level graphics abstractions built on NVRHI
+
+#include "types.h"
+#include "context.h"
+#include "vertex_buffer.h"
+#include "index_buffer.h"
+#include "uniform_buffer.h"
+#include "texture.h"
+#include "sampler.h"
+#include "render_target.h"
+#include "shader.h"
+#include "input_layout.h"
+#include "binding_layout.h"
+#include "pipeline.h"
+#include "renderer2d.h"
