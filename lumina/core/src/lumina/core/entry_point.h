5068505666b6fde267ea147391fca2c8c94a3c35
Stephen Watson <imstephen.1sw@gmail.com>
2026-02-16 16:13:30 -0500

    fix: add exception safety to application entry point
      Wrap application pointer in std::unique_ptr to ensure proper cleanup
      if app->run() throws an exception. Also removed redundant identical
      #ifdef LUMINA_PLATFORM_WINDOWS branches to simplify the code.

--- lumina/core/src/lumina/core/entry_point.h
+++ lumina/core/src/lumina/core/entry_point.h
@@ -2,28 +2,13 @@
 
 #include "application.h"
 
-#ifdef LUMINA_PLATFORM_WINDOWS
+#include <memory>
 
 extern lumina::core::application* lumina::core::create_application(int argc, char** argv);
 
 int main(int argc, char** argv)
 {
-    auto app = lumina::core::create_application(argc, argv);
+    std::unique_ptr<lumina::core::application> app(lumina::core::create_application(argc, argv));
     app->run();
-    delete app;
     return 0;
 }
-
-#else
-
-extern lumina::core::application* lumina::core::create_application(int argc, char** argv);
-
-int main(int argc, char** argv)
-{
-    auto app = lumina::core::create_application(argc, argv);
-    app->run();
-    delete app;
-    return 0;
-}
-
-#endif
